5db35fef7f4e56bbd4cb9c8b376b2aac558cf672
Vasiliy Mozhaev <mozhay2005@gmail.com>
2025-11-08 15:43:31 +0300

    feat: server dependencies

---
+++ server/src/server/config.py
@@ -0,0 +1,19 @@
+from functools import cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    db_driver: str
+    db_name: str
+    db_username: str
+    db_password: str
+    db_host: str
+    db_port: int
+
+    model_config = SettingsConfigDict(env_file=None)
+
+
+@cache
+def get_settings() -> Settings:
+    return Settings()  # type: ignore
