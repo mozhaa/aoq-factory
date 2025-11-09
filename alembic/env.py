6100c8584521610cf7de7eb8c933ff22f1146f7f
Vasiliy Mozhaev <mozhay2005@gmail.com>
2025-11-09 11:19:23 +0300

    feat: change alembic/env.py to see metadata and to connect to db

--- alembic/env.py
+++ alembic/env.py
@@ -1,11 +1,11 @@
 import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import pool
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
+from aoq_factory.database.connection import get_async_engine, get_url
+from aoq_factory.database.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,9 +18,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -40,7 +38,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -65,11 +63,7 @@ async def run_async_migrations() -> None:
 
     """
 
-    connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = get_async_engine(get_url(), echo=True)
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
