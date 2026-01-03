c78a7f4c55e4e75d4148d62eb4b706df6f4764e4
mozhaa <mozhay2005@gmail.com>
2026-01-03 13:28:07 +0300

    chore(config): remove unused values from config

--- src/aoq_factory/config.py
+++ src/aoq_factory/config.py
@@ -9,11 +9,6 @@ class Settings(BaseSettings):
     db_password: str
     db_host: str
     db_port: int
-    resources_dir: str
-    anidb_request_interval: float
-    idsmoe_api_key: str
-    idsmoe_rate_limiter_max_rate: float
-    idsmoe_rate_limiter_time_period: float
 
     model_config = SettingsConfigDict(env_file=None)
 
